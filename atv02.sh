65fb9e60d813084798438edb8c88f5974d1b90ec
And Paiva <andre.pcr@hotmail.com>
2025-01-11 20:55:55 -0300

    Atividade 2 (WIP)

---
+++ atv02.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export SVGA_VGPU10=0
+source /ros_entrypoint.sh
+
+if [ -d "Atividade_02/build" ]; then
+    rm -rf Atividade_02/build
+fi
+
+if [ -d "Atividade_02/devel" ]; then
+    rm -rf Atividade_02/devel
+fi
+
+cd Atividade_02 && catkin_make && cd ..
+
+source Atividade_02/devel/setup.bash
+source Atividade_02/src/husky_accessories.sh
+
+rosdep update
+rosdep install --from-paths Atividade_02/src --ignore-src -r -y --rosdistro noetic
+
+roslaunch lar_gazebo lar_husky_amcl.launch
